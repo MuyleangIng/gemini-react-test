1bfffa01c58a075d6ba05dde84308c3b81dc8330
sen <sen@sens-Mac-mini.local>
2024-09-13 08:04:31 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GeminiChat from '/src/components/GeminiChat'
+import GeminiChatBot from './components/GeminiChatBot'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <GeminiChat /> */}
+      <GeminiChatBot />
+    </div>
+  )
+}
+
+export default App
